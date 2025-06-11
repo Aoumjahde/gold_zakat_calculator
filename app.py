50f49a04a5b8daf40f82fd68ef59cba74e696369
jaferpilakkal <pjaferparappoor@gmail.com>
2025-06-11 12:07:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ if st.button("Calculate"):
         st.write(f"Value of Total Zakat Paid in INR: **₹{sum(total_zakat_paid_value):,.2f}**")
         st.write(f"Gold Currently Holding in Grams: **{current_gold_grams:,.2f}g**")
         
-        st.markdown("<br><h5>Growth and Appreciation</h5>", unsafe_allow_html=True)
+        st.markdown("<br><h5 style='margin-top: -10px;'>Growth and Appreciation</h5>", unsafe_allow_html=True)
 
         st.write(f"Final Value of Gold Currently Holding: **₹{value:,.2f}**")
         st.write(f"Total Appreciation (After Zakat): **{total_appreciation:.2f}%**")
